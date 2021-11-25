3dc2b5e9181848e19437753fd2dabe931355c2ff
Leonard <trufelnaisveni@gmail.com>
2021-11-25 17:55:12 +0300

    Update main

--- Zadacha1/Zadacha1/Zadacha1.cpp
+++ Zadacha1/Zadacha1/Zadacha1.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
-
+    while (true) {
+        int a;
+        cout << "1 - Создать файл и записать в него строку" << endl << "2 - Заархивировать файл" << endl << "3 - Разархивировать файл" << endl << "Ваш выбор = ";
+        cin >> a;
+        switch (a) {
+        case 1: { cout << "Функция в разработке" << endl; break; }
+        case 2: { Encrypt(s); break; }
+        case 3: { cout << "Функция в разработке" << endl; break; }
+        }
+    }
 }
