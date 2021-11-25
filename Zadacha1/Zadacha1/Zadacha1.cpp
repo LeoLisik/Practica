a16574d43a3a4874386fb2dfa15c50162794eb20
Leonard <trufelnaisveni@gmail.com>
2021-11-25 21:21:25 +0300

    update 0.1

--- Zadacha1/Zadacha1/Zadacha1.cpp
+++ Zadacha1/Zadacha1/Zadacha1.cpp
@@ -1,27 +1,31 @@
 #include <Windows.h>
 #include "Header.h"
 #include <fstream>
+#include <locale.h>
+#define ch enterStr.find_last_not_of("-0123456789.")
+#define ch2 enterStr.find_last_of("-")
+#define ch3  enterStr.find_last_of(".")-enterStr.find_first_of(".")
 
-void Encrypt() { //Р¤СѓРЅРєС†РёСЏ Р·Р°С€РёС„СЂРѕРІРєРё СЃС‚СЂРѕРєРё
+void Encrypt() { //Функция зашифровки строки
     string inputString, outputString;
-    fstream file ("input.txt"); //РћС‚РєСЂС‹С‚СЊ С„Р°Р№Р»Р° РІРІРѕРґР°
-    if (!file.is_open()) {cout << "РћС€РёР±РєР° 1: Р¤Р°Р№Р» РЅРµ РѕС‚РєСЂС‹Р»СЃСЏ. РџРѕРїСЂРѕР±СѓР№С‚Рµ РґР»СЏ РЅР°С‡Р°Р»Р° РµРіРѕ СЃРѕР·РґР°С‚СЊ." << endl; return;} //РџСЂРѕРІРµСЂРєР° РѕС‚РєСЂС‹С‚РёСЏ С„Р°Р№Р»Р°
-    getline(file, inputString); //РЎС‡РёС‚Р°С‚СЊ СЃС‚СЂРѕРєСѓ РёР· С„Р°Р№Р»Р°
-    file.close(); //Р—Р°РєСЂС‹С‚СЊ С„Р°Р№Р»
-    for (int i = 0, counterSameSimbols; i < inputString.length(); i += counterSameSimbols - 1) { //Р¦РёРєР» РїРѕР»СѓС‡Р°СЋС‰РёР№ РєР°Р¶РґС‹Р№ СЂР°Р· РЅРѕРІС‹Р№ СЃРёРјРІРѕР»
+    fstream file ("input.txt"); //Открыть файла ввода
+    if (!file.is_open()) {cout << "Ошибка 1: Файл не открылся. Попробуйте для начала его создать." << endl; return;} //Проверка открытия файла
+    getline(file, inputString); //Считать строку из файла
+    file.close(); //Закрыть файл
+    for (int i = 0, counterSameSimbols; i < inputString.length(); i += counterSameSimbols - 1) { //Цикл получающий каждый раз новый символ
         counterSameSimbols = 0; 
-        outputString += inputString[i]; //Р—Р°РїРёСЃСЊ СЃРёРјРІРѕР»Р° РІ РёС‚РѕРіРѕРІСѓСЋ СЃС‚СЂРѕРєСѓ 
-        for (int i1 = i++; inputString[i] == inputString[i1]; i1++, counterSameSimbols++) {} //РЎС‡С‘С‚С‡РёРє РєРѕР»РёС‡РµСЃС‚РІР° РѕРґРёРЅР°РєРѕРІРѕРіРѕ СЃРёРјРІРѕР»Р°
-        if (counterSameSimbols == 0) { counterSameSimbols++; } //Р•СЃР»Рё СЃРёРјРІРѕР» РІСЃРµРіРѕ РѕРґРёРЅ, С‚Рѕ СѓРІРµР»РёС‡РёС‚СЊ counterSameSimbols РЅР° 1
-        if (counterSameSimbols > 1) { //Р•СЃР»Рё РїРѕРІС‚РѕСЂРѕРІ СЃРёРјРІРѕР»Р° Р±РѕР»СЊС€Рµ 1, С‚Рѕ Р·Р°РїРёСЃС‹РІР°С‚СЊ С†РёС„СЂСѓ
+        outputString += inputString[i]; //Запись символа в итоговую строку 
+        for (int i1 = i++; inputString[i] == inputString[i1]; i1++, counterSameSimbols++) {} //Счётчик количества одинакового символа
+        if (counterSameSimbols == 0) { counterSameSimbols++; } //Если символ всего один, то увеличить counterSameSimbols на 1
+        if (counterSameSimbols > 1) { //Если повторов символа больше 1, то записывать цифру
             outputString += to_string(counterSameSimbols);
         }
     }
-    file.open("output.txt"); //РћС‚РєСЂС‹С‚РёРµ С„Р°Р№Р»Р° РІС‹РІРѕРґР°
-    if (!file.is_open()) { cout << "РћС€РёР±РєР° 1: Р¤Р°Р№Р» РЅРµ РѕС‚РєСЂС‹Р»СЃСЏ. РџРѕРїСЂРѕР±СѓР№С‚Рµ РґР»СЏ РЅР°С‡Р°Р»Р° РµРіРѕ СЃРѕР·РґР°С‚СЊ." << endl; return; } //РџСЂРѕРІРµСЂРєР° РѕС‚РєСЂС‹С‚РёСЏ С„Р°Р№Р»Р°
-    file << outputString; //Р—Р°РїРёСЃР°С‚СЊ РёС‚РѕРі РІ С„Р°Р№Р»
-    file.close(); //Р—Р°РєСЂС‹С‚СЊ С„Р°Р№Р»
-    cout << endl << endl << endl << "РђСЂС…РёРІР°С†РёСЏ СѓСЃРїРµС€РЅРѕ РІС‹РїРѕР»РЅРµРЅР°" << endl << endl << endl << endl;
+    file.open("output.txt"); //Открытие файла вывода
+    if (!file.is_open()) { cout << "Ошибка 1: Файл не открылся. Попробуйте для начала его создать." << endl; return; } //Проверка открытия файла
+    file << outputString; //Записать итог в файл
+    file.close(); //Закрыть файл
+    cout << endl << endl << endl << "Архивация успешно выполнена" << endl << endl << endl << endl;
 }
 
 int main()
@@ -29,15 +33,17 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     int endProgram = 0; 
-    while (endProgram == 0) { //Р¦РёРєР» СЂР°Р±РѕС‚С‹ РїСЂРѕРіСЂР°РјРјС‹
-        int enter;
-        cout << "1 - РЎРѕР·РґР°С‚СЊ С„Р°Р№Р» Рё Р·Р°РїРёСЃР°С‚СЊ РІ РЅРµРіРѕ СЃС‚СЂРѕРєСѓ" << endl << "2 - Р—Р°Р°СЂС…РёРІРёСЂРѕРІР°С‚СЊ С„Р°Р№Р»" << endl << "3 - Р Р°Р·Р°СЂС…РёРІРёСЂРѕРІР°С‚СЊ С„Р°Р№Р»" << endl << "0 - Р’С‹С…РѕРґ РёР· РїСЂРѕРіСЂР°РјРјС‹" << endl << "Р’Р°С€ РІС‹Р±РѕСЂ = ";
-        cin >> enter;
-        switch (enter) { //РџРµСЂРµРєР»СЋС‡Р°С‚РµР»СЊ РІС‹Р±РѕСЂРѕРІ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ
-        case 1: { cout << "Р¤СѓРЅРєС†РёСЏ РІ СЂР°Р·СЂР°Р±РѕС‚РєРµ" << endl; break; } //Р—Р°РїСѓСЃРє С„СѓРЅРєС†РёСЋ СЃРѕР·РґР°РЅРёСЏ С„Р°Р№Р»РѕРІ Рё Р·Р°РїРёСЃРё РІ С„Р°Р№Р»
-        case 2: { Encrypt(); break; } //Р—Р°РїСѓСЃРє С„СѓРЅРєС†РёРё Р°СЂС…РёРІР°С†РёРё С„Р°Р№Р»Р°
-        case 3: { cout << "Р¤СѓРЅРєС†РёСЏ РІ СЂР°Р·СЂР°Р±РѕС‚РєРµ" << endl; break; } //Р—Р°РїСѓСЃРє С„СѓРЅРєС†РёРё СЂР°Р·Р°СЂС…РёРІР°С†РёРё С„Р°Р№Р»Р°
-        case 0: { endProgram = 1; break; } //Р’С‹С…РѕРґ РёР· РїСЂРѕРіСЂР°РјРјС‹
+    while (endProgram == 0) { //Цикл работы программы
+        int enterInt;
+        string enterStr;
+        cout << "1 - Создать файл и записать в него строку" << endl << "2 - Заархивировать файл" << endl << "3 - Разархивировать файл" << endl << "0 - Выход из программы" << endl << "Ваш выбор = ";
+        getline(cin, enterStr);
+        if ((ch != -1) or (ch2 != -1 and ch2 != 0) or (ch3 != 0)) { enterInt = 9; } else { enterInt = atof(enterStr.c_str()); }
+        switch (enterInt) { //Переключатель выборов пользователя
+        case 1: { cout << "Функция в разработке" << endl; break; } //Запуск функцию создания файлов и записи в файл
+        case 2: { Encrypt(); break; } //Запуск функции архивации файла
+        case 3: { cout << "Функция в разработке" << endl; break; } //Запуск функции разархивации файла
+        case 0: { endProgram = 1; break; } //Выход из программы
         }
     }
 }
