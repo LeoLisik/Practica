5df877d2fad32915a7ca288d8fdf5ccb468176a9
Leonard <trufelnaisveni@gmail.com>
2021-11-29 15:05:24 +0300

    Version 1.2

--- Zadacha1/Zadacha1/Zadacha1.cpp
+++ Zadacha1/Zadacha1/Zadacha1.cpp
@@ -74,7 +74,7 @@ void Archive() { //Функция архивирования строки
             outputString += to_string(counterSameSimbols);
         }
     }
-    file.open("output.txt"); //Открытие файла вывода
+    file.open("output.txt", ios::out); //Открытие файла вывода
     if (!file.is_open()) { //Проверка открытия файла
         cout << "\n" << "\x1b[31mОшибка 1: Файл не открылся. Попробуйте для начала его создать.\x1b[0m" << "\n\n\n\n"; 
         return; 
@@ -85,65 +85,46 @@ void Archive() { //Функция архивирования строки
 }
 
 void Dearchive() {
-    string CountStringOutput, CountOutput, SimvolCountOutput, StringNumbersLetter, Dearchive = "";
-    int NumbersLetter, ProverkaNumbers = 0;
-    char  SimvolCount;
-    ifstream Read("output.txt"); //создаём поток файла + открываем файл output.txt для чтения 
-    if (!Read) { //Проверка открытия файла
+    string inputString, fullNumberStr, outputString = "";
+    int fullNumber;
+    fstream file("output.txt", ios::in);
+    if (!file) { //Проверка открытия файла
         cout << "Ошибка открытия файла output.txt. Возможно его не существует."; 
         return;
     }
-    getline(Read, CountOutput);
-    cout << "Было:" << CountOutput << endl;
-    Read.close();//закрываем файл
-    for (int i = 0; i < CountOutput.length(); i++) { //цикл, повторяющийся столько раз, сколько в строке CountOutput символов
-        SimvolCountOutput.replace(0, 2, CountOutput, i, 1); //заменяем символы в строке SimvolCountOutput на каждый следующий символ (i) из строки CountOutput
-        SimvolCount = SimvolCountOutput[0]; //присваиваем символ из строки SimvolCountOutput в переменную символ SimvolCount
-        //проверка№1 было ли прошлое число двухзначным(или больше):
-        if (SimvolCount >= '0' and SimvolCount <= '9' and ProverkaNumbers == 1) { //условие, если SimvolCount является цифрой и ProverkaNumbers равняется одному, то..
-            ProverkaNumbers = 0; //..то ProverkaNumbers приравнивается нулю
-            continue; //цикл начинает работать сначала, при этом к i прибавляется 1, а не обнуляется
+    getline(file, inputString);
+    cout << "Было:" << inputString << endl;
+    file.close();
+    for (int i = 0; i < inputString.length(); i++) { //Цикл перебирает каждый символ
+        if (inputString[i] >= '0' and inputString[i] <= '9' and inputString[i - 1] >= '0' and inputString[i - 1] <= '9') { //Если предыдущий символ число и текущий, то пропустить итерацию
+            continue; 
         }
-        //защита - является ли первый символ в файле цифрой:
-        if (SimvolCount >= '0' and SimvolCount <= '9' and i == 0) { //условие, если SimvolCount является цифрой и i равняется 0, то..
+        if (inputString[i] >= '0' and inputString[i] <= '9' and i == 0) { //Если первый символ цифра
             cout << "Ошибка. Первый символ в файле - цифра. Такого не может быть.";
-            return; //закончить работу функции
+            return; 
         }
-        if (SimvolCount >= '0' and SimvolCount <= '9') { //условие, если SimvolCount является цифрой
-            StringNumbersLetter += SimvolCountOutput; //добавляем к строке StringNumbersLetter строку SimvolCountOutput, состоящую из одного символа
-            SimvolCountOutput.replace(0, 2, CountOutput, i + 1, 1); //заменяем символы в строке SimvolCountOutput на следующий символ (i+1) из строки CountOutput
-            SimvolCount = SimvolCountOutput[0]; //присваиваем символ из строки SimvolCountOutput в переменную символ SimvolCount
-            //проврка№2 является ли число двухзначным(или больше):
-            for (int j = 1; SimvolCount >= '0' and SimvolCount <= '9'; j++) { //цикл, повторяющийся пока SimvolCount является числом
-                ProverkaNumbers = 1; //приравниваем число ProverkaNumbers одному для проверки№1
-                StringNumbersLetter += SimvolCountOutput; //добавляем к строке StringNumbersLetter строку SimvolCountOutput, состоящую из одного символа
-                SimvolCountOutput.replace(0, 2, CountOutput, i + 2, 1); //заменяем символы в строке SimvolCountOutput на следующий символ (i+1) из строки CountOutput
-                SimvolCount = SimvolCountOutput[0]; //присваиваем символ из строки SimvolCountOutput в переменную символ SimvolCount
+        if (inputString[i] >= '0' and inputString[i] <= '9') { //Если символ - цифра
+            fullNumberStr += inputString[i]; //добавляем к строке fullNumberStr текущее число
+            for (int j = 1; inputString[i + j] >= '0' and inputString[i + j] <= '9'; j++) { //Пока текущий символ - число
+                fullNumberStr += inputString[i + j]; //Добавлять число в строку полного числа
             }
-            //защита - если произошло исключение и в строке StringNumbersLetter есть символы, кроме цифр
-            if (StringNumbersLetter.find_last_not_of("123456780") != -1) { //условие, если в строке StringNumbersLetter есть символы, кроме цифр
-                cout << "Ошибка деархивации";
-                return; //закончить работу функции
+            fullNumber = stoi(fullNumberStr); //перевод всех чисел в единую цифру
+            for (int j = 1; j < fullNumber; j++) { //Пока количество букв меньше уже поставленных
+                outputString += inputString[i - 1]; //Добавлять букву в итог
             }
-            NumbersLetter = stoi(StringNumbersLetter); //переводим строку StringNumbersLetter в число NumbersLetter
-            for (int j = 1; j < NumbersLetter; j++) { //цикл, повторяющийся NumbersLetter-раз
-                Dearchive += CountOutput[i - 1]; //в строку Dearchive вставляем символ, который был до числа
-            }
-            StringNumbersLetter.clear(); //очищаем строку StringNumbersLetter
-        }
-        else { //иначе..
-            Dearchive += SimvolCountOutput; //добавляем символ (по счётчику i) из строки SimvolCountOutput в строку Dearchive
+            fullNumberStr.clear(); //Очистка fullNumberStr для следующей итерации
+        } else { 
+            outputString += inputString[i]; //Добавить текущую букву в итог
         }
     }
-    cout << "Итог:" << Dearchive;
-    ofstream Write("output.txt", ios::trunc); //создаём поток файла + открываем файл output.txt для перезаписывания данных 
-    //защита - на открытие файла:
-    if (!Write.is_open()) { //условие, если файл не открылся, то..
+    cout << "Итог:" << outputString << endl;
+    file.open("output.txt", ios::out); 
+    if (!file) { //Проверка открытия файла
         cout << "Ошибка открытия файла. Возможно его не существует.";
-        return; //..завершения работы функции
+        return;
     }
-    Write << Dearchive; //запись строки Dearchive в файл output.txt
-    Write.close(); //закрываем файла
+    file << outputString; //Записать итог в файл
+    file.close(); 
 }
 
 int main()
@@ -159,7 +140,7 @@ int main()
         cout << endl << endl;
         if ((ch != -1) or (ch2 != -1 and ch2 != 0) or (ch3 != 0) or (enterStr.empty()) or (ch2 == 0)) { cout << "\x1b[31mОшибка 0: Некоректный ввод данных\x1b[0m" << endl; enterInt = 9; }
         else { enterInt = atof(enterStr.c_str()); }
-        if (enterInt < 0 or enterInt > 3 and enterInt != 9) { cout << "\x1b[31mОшибка 2: Такой команды не существует\x1b[0m" << endl; }
+        if (enterInt < 0 or enterInt > 3 and enterInt != 9) { cout << "\x1b[31mОшибка 2: Такой команды не существует\x1b[0m" << endl; } 
         switch (enterInt) { //Переключатель выбора пользователя
         case 1: { CreateInput(); break; } //Запуск функции создания файлов
         case 2: { Archive(); break; } //Запуск функции зашифровки файлов
