b80509d83abebaec7e3c05c4dd190b666c2e7ebb
Leonard <trufelnaisveni@gmail.com>
2021-12-13 18:52:27 +0300

    End

--- Zadacha1/Zadacha1/Header.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#define ch enterStr.find_last_not_of("-0123456789.")
-#define ch2 enterStr.find_last_of("-")
-#define ch3  enterStr.find_last_of(".")-enterStr.find_first_of(".")
-using namespace std;
-void debug(string s);
-void debug(int x);
